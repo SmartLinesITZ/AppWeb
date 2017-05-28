ac148902a937f95b84b5d6d3f60a6c8ed05903fe
AlexGT <isc.agt@gmail.com>
2017-05-28 03:35:28 -0500

    Nuevo Index

---
+++ carusel.php
@@ -0,0 +1,27 @@
+
+
+<div class="slider z-depth-4 main">
+  <ul class="slides">
+    <li>
+      <img src="images/1.jpg">
+      <div class="caption right-align">
+        <h3>Gran catalogo</h3>
+        <h5>Mas de 100 Sucursales</h5>
+      </div>
+    </li>
+    <li>
+      <img src="images/2.jpg">
+      <div class="caption right-align">
+        <h3>Aparta tu lugar</h3>
+        <h5>Pide tu comida antes de llegar o aparta lugar</h5>
+      </div>
+    </li>
+    <li>
+      <img src="images/3.jpg">
+      <div class="caption right-align">
+        <h3>Servicio a Domicilio</h3>
+        <h5>Tu comida a domicilio</h5>
+      </div>
+    </li>
+  </ul>
+</div>
---
+++ menuNoSession.php
@@ -0,0 +1,19 @@
+<nav class="z-depth-2 amber accent-4" role="navigation">
+  <div class="nav-wrapper container">
+   	<a href="#" class="brand-logo">Click to Eat</a>
+
+   	<ul id="nav-wrapper" class="right hide-on-med-and-down">
+   		<li><a class="waves-effect waves-yellow pulse" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+   		<li><a class="waves-effect waves-yellow pulse" href="#modal1"><i class="material-icons left">account_circle</i>Iniciar Sesión</a></li>
+   		<li><a class="waves-effect waves-yellow pulse" href="about.php"><i class="material-icons left">favorite</i>Conocenos</a></li>
+   	</ul>
+    <ul id="nav-mobile" class="side-nav">
+      <div class="card-panel amber accent-4">Click to Eat</div>
+        <li><a href="index.php" class="waves-effect waves-yellow pulse" ><i class="material-icons">home</i>Inicio</a></li>
+   		  <li><a class="waves-effect waves-yellow pulse" href="#modal1"><i class="material-icons left">account_circle</i>Iniciar Sesión</a></li>
+   		  <li><a class="waves-effect waves-yellow pulse" href="about.php"><i class="material-icons left">favorite</i>Conocenos</a></li>
+    </ul>
+    <a href="#" data-activates="nav-mobile" class="button-collapse">
+    <i class="material-icons">menu</i></a>
+  </div>
+</nav>
